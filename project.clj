01bd221dca887f2a47d1d4b983a88b7eec2996ba
ZStriker19 <zgroves19@gmail.com>
2020-06-22 12:39:01 -0600

    datadog agent runs in debug with app

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.1"]
                  [ring/ring-jetty-adapter "1.1.2"]]
-  :main ^:skip-aot sample.app)
+:java-agents [[com.datadoghq/dd-java-agent "0.55.0"]]
+:jvm-opts ["-Ddd.trace.debug=true"]
+:main ^:skip-aot sample.app)
